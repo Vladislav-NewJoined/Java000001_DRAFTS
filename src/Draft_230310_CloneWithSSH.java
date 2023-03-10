09a80f896fcb0a50df45668a4eb2ecb06fe9c796
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-10 19:46:53 +0300

    Initial commit

---
+++ src/Draft_230310_CloneWithSSH.java
@@ -0,0 +1,10 @@
+/* Как скачать проект в Intellij IDEA с GitHub !!! ИМЕННО С КЛЮЧЕМ SSH ЗДЕСЬ
+https://youtu.be/OYhPpeufgW8   минута 4.50
+или здесь
+https://www.youtube.com/watch?v=OYhPpeufgW8&t=25s */
+public class Draft_230310_CloneWithSSH {
+    public static void main(String[] args) {
+        System.out.println("Как скачать проект в Intellij IDEA с GitHub !!! ИМЕННО С КЛЮЧЕМ SSH ЗДЕСЬ https://youtu.be/OYhPpeufgW8   минута 4.50 или здесь https://www.youtube.com/watch?v=OYhPpeufgW8&t=25s");
+    }
+
+}
