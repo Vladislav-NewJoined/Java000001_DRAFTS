f44ac5d26fc3061639cc1dc1f1aecaef02511437
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-12 12:37:43 +0300

    Initial commit

---
+++ src/Draft_230312_2.java
@@ -0,0 +1,17 @@
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Draft_230312_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //new InputStreamReader
+        System.out.print("Input a Dream: ");
+        String dream_input = scanner.next();
+        System.out.print("Input a Status: ");
+        String s_status_input = scanner.next();
+        System.out.print("Input a Line: ");
+        String line_input = scanner.next();
+        /* System.out.println(dream_input);
+        System.out.println(s_status_input);
+        System.out.println(line_input);*/
+    }
+}
---
+++ src/Draft_230312_3_inOneLine.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Draft_230312_3_inOneLine {
+    public static void main(String[] args) {
+        Scanner inWord = new Scanner(System.in);
+        int n = inWord.nextInt();
+        for (int i = 0; i < n; i++) {
+            //if (i % 2 == 0) {
+            System.out.print(i + " ");
+        }
+    }
+}
